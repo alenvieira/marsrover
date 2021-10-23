dd919373b19c2d50aa4f4445f8817f0fc7aff302
Alen Vieira <alencv@gmail.com>
2021-10-23 17:20:41 -0300

    create function rotate for left and right

--- test_rover.py
+++ test_rover.py
@@ -7,6 +7,26 @@ class Rover:
         self.y = y
         self.orientation = orientation
 
+    def rotate_left(self):
+        if self.orientation == "N":
+            self.orientation = "W"
+        elif self.orientation == "W":
+            self.orientation = "S"
+        elif self.orientation == "S":
+            self.orientation = "E"
+        elif self.orientation == "E":
+            self.orientation = "N"
+
+    def rotate_right(self):
+        if self.orientation == "N":
+            self.orientation = "E"
+        elif self.orientation == "E":
+            self.orientation = "S"
+        elif self.orientation == "S":
+            self.orientation = "W"
+        elif self.orientation == "W":
+            self.orientation = "N"
+
 
 class RoverTestCase(unittest.TestCase):
     def setUp(self):
@@ -15,4 +35,24 @@ class RoverTestCase(unittest.TestCase):
     def test_create_rover(self):
         self.assertEqual(self.rover.x, 1)
         self.assertEqual(self.rover.y, 2)
+        self.assertEqual(self.rover.orientation, "N")
+
+    def test_rotate_left(self):
+        self.rover.rotate_left()
+        self.assertEqual(self.rover.orientation, "W")
+        self.rover.rotate_left()
+        self.assertEqual(self.rover.orientation, "S")
+        self.rover.rotate_left()
+        self.assertEqual(self.rover.orientation, "E")
+        self.rover.rotate_left()
+        self.assertEqual(self.rover.orientation, "N")
+
+    def test_rotate_right(self):
+        self.rover.rotate_right()
+        self.assertEqual(self.rover.orientation, "E")
+        self.rover.rotate_right()
+        self.assertEqual(self.rover.orientation, "S")
+        self.rover.rotate_right()
+        self.assertEqual(self.rover.orientation, "W")
+        self.rover.rotate_right()
         self.assertEqual(self.rover.orientation, "N")
